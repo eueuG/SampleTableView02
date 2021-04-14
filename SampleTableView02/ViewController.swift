dbc6bb394d093c58eb31d2af417817898b483ecc
rinetrous <rinellie1228@icloud.com>
2021-04-14 11:31:32 +0900

    Initial Commit

---
+++ SampleTableView02/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleTableView02
+//
+//  Created by 野田凜太郎 on 2021/04/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
